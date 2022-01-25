d4ad54ad3aeef072b3f1cd4e1b1ad52d7590f764
Angela De Pasquale <angeladepasquale_@libero.it>
2022-01-25 16:15:13 +0100

    Aggiunti risultati del gioco

--- main.js
+++ main.js
@@ -29,5 +29,19 @@ buttonGame.addEventListener('click',
         const GamePart = document.querySelector('.start-game');
         GamePart.classList.add('active');
 
+        // Inserimento del tuo numero random
+        let yourNumber =  Math.floor(Math.random() * (6 - 1 +1)) +1;
+        document.getElementById('rnd-your-number').innerHTML = yourNumber;
+
+        // Inserimento delnumero random del PC
+        let pcNumber =  Math.floor(Math.random() * (6 - 1 +1)) +1;
+        document.getElementById('rnd-pc-number').innerHTML = pcNumber;
+
+        //Vittoria 
+        if (yourNumber > pcNumber) {
+            document.getElementById('victory-results').innerHTML = "HAI VINTO!"
+        } else {
+            document.getElementById('victory-results').innerHTML = "HAI PERSO!"
+        }
     }
 )
