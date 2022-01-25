d7a98539c5491aac9c68e2105a763f88c69ff3a0
Angela De Pasquale <angeladepasquale_@libero.it>
2022-01-25 15:25:43 +0100

    Creazione esercizio

---
+++ main.js
@@ -0,0 +1,21 @@
+
+const login = [ 'mickeymouse@gmail.com', 'donald.duck@gmail.com', 'goofygoof@gmail.com'];
+const loginEmail = document.getElementById('email');
+console.log(loginEmail);
+
+let buttonLogin = document.getElementById('login');
+
+buttonLogin.addEventListener('click',
+    function() {
+
+        for (let i = 0; i < login.length; i++) {
+
+            if (login[i] == loginEmail.value) {
+                alert('Sei autorizzato ad accedere, ora sfida il nostro computer!');
+            } else if (login[i] != loginEmail.value) {
+                alert('Accesso non effettuato: ricontrolla la tua mail!');
+            }
+        }
+
+    }
+)
